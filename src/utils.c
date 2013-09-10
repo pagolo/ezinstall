4c657850992b43aedc60d4ee80bad9eb7901198e
Paolo Bozzo <pagolo.bozzo@gmail.com>
2013-09-10 19:25:26 +0200

    popenRWE by Trojanovski added

--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,8 @@
 #include "main.h"
 #include <dirent.h>
 #include <sys/param.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #ifndef HAVE_MEMMEM
 
@@ -332,3 +334,105 @@ void ChangePermissions(void) {
   if (globaldata.gd_inidata->perm_list && globaldata.gd_loglevel > LOG_NONE)
     WriteLog(_("permissions setup"));
 }
+
+/**
+ * Copyright 2009-2010 Bart Trojanowski <bart@jukie.net>
+ * Licensed under GPLv2, or later, at your choosing.
+ *
+ * bidirectional popen() call
+ *
+ * @param rwepipe - int array of size three
+ * @param exe - program to run
+ * @param argv - argument list
+ * @return pid or -1 on error
+ *
+ * The caller passes in an array of three integers (rwepipe), on successful
+ * execution it can then write to element 0 (stdin of exe), and read from
+ * element 1 (stdout) and 2 (stderr).
+ */
+static int popenRWE(int *rwepipe, const char *exe, const char *const argv[]) {
+  int in[2];
+  int out[2];
+  int err[2];
+  int pid;
+  int rc;
+
+  rc = pipe(in);
+  if (rc < 0)
+    goto error_in;
+
+  rc = pipe(out);
+  if (rc < 0)
+    goto error_out;
+
+  rc = pipe(err);
+  if (rc < 0)
+    goto error_err;
+
+  pid = fork();
+  if (pid > 0) { // parent
+    close(in[0]);
+    close(out[1]);
+    close(err[1]);
+    rwepipe[0] = in[1];
+    rwepipe[1] = out[0];
+    rwepipe[2] = err[0];
+    return pid;
+  } else if (pid == 0) { // child
+    close(in[1]);
+    close(out[0]);
+    close(err[0]);
+    close(0);
+    dup(in[0]);
+    close(1);
+    dup(out[1]);
+    close(2);
+    dup(err[1]);
+
+    execvp(exe, (char**) argv);
+    exit(1);
+  } else
+    goto error_fork;
+
+  return pid;
+
+error_fork:
+  close(err[0]);
+  close(err[1]);
+error_err:
+  close(out[0]);
+  close(out[1]);
+error_out:
+  close(in[0]);
+  close(in[1]);
+error_in:
+  return -1;
+}
+
+static int pcloseRWE(int pid, int *rwepipe) {
+  int rc, status;
+  close(rwepipe[0]);
+  close(rwepipe[1]);
+  close(rwepipe[2]);
+  rc = waitpid(pid, &status, 0);
+  return status;
+}
+
+/**
+ * example
+ int pipe[3];
+    int pid;
+    const char *const args[] = {
+            "cat",
+            "-n",
+            NULL
+    };
+
+    pid = popenRWE(pipe, args[0], args);
+
+    // write to pipe[0] - input to cat
+    // read from pipe[1] - output from cat
+    // read from pipe[2] - errors from cat
+
+    pcloseRWE(pid, pipe);
+ */
